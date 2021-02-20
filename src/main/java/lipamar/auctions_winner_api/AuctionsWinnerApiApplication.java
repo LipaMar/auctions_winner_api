29d309ea831807eabe016819abcff965af8d63b0
Marcin Lipa <marcin.lipa@onet.pl>
2021-02-20 10:04:34 +0100

    Initial commit

---
+++ src/main/java/lipamar/auctions_winner_api/AuctionsWinnerApiApplication.java
@@ -0,0 +1,13 @@
+package lipamar.auctions_winner_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionsWinnerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionsWinnerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lipamar/auctions_winner_api/AuctionsWinnerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lipamar.auctions_winner_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionsWinnerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
